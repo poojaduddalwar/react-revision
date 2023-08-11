75039b74ffffc086f39f8ca1a05fec982152c8d4
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2023-08-11 23:43:23 +0530

    components created using functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 
 let skiData = {
@@ -7,28 +7,25 @@ let skiData = {
   backCountry: 20,
   goal: 100
 }
-class SkiDaysCounter extends Component {
 
-
-  getPersent = decimal => {
-    return decimal * 100 + '%'
-  }
-  calGoal = (total, goal) => {
-    return this.getPersent(total / goal)
-  }
-  render() {
-    const { total, powder, backCountry, goal } = this.props
-    return (
-      <section>
-        <div>Total : {total}</div>
-        <div>Powder : {powder}</div>
-        <div>Back Country : {backCountry}</div>
-        <div>Goal Progress: {this.calGoal(total, goal)}</div>
-      </section>
-    )
-  }
+const getPersent = decimal => {
+  return decimal * 100 + '%'
+}
+const calGoal = (total, goal) => {
+  return getPersent(total / goal)
 }
 
+const SkiDaysCounter = ({ total, powder, backCountry, goal }) => {
+
+  return (
+    <section>
+      <div>Total : {total}</div>
+      <div>Powder : {powder}</div>
+      <div>Back Country : {backCountry}</div>
+      <div>Goal Progress: {calGoal(total, goal)}</div>
+    </section>
+  )
+}
 
 render(
   <SkiDaysCounter total={skiData.total} powder={skiData.powder} backCountry={skiData.backCountry} goal={skiData.goal} />,
